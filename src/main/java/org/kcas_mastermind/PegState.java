3bff94a72aca77b395853c644456c273ac2efcf0
Khanh Cao Ngoc Mai <knc006@bucknell.com>
2025-03-21 11:56:06 -0400

    PegState and gamConfig

---
+++ src/main/java/org/kcas_mastermind/PegState.java
@@ -0,0 +1,20 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Spring 2025
+ *
+ * Name: Khanh Cao
+ * Date: 3/20/2025
+ * Time: 11:25 AM
+ *
+ * Project: csci205_hw
+ * Package: org.kcas_mastermind
+ * Class: PegState
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+
+package org.kcas_mastermind;
+public class PegState {
+}
